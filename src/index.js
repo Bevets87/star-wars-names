f53b6264c3cbff0e1ab8c5330d3fb905ab67c9ba
Bevets87 <Bevets87@gmail.com>
2019-11-27 01:36:15 -0500

    Add names module

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import unique from 'unique-random-array'
+import json from './star-wars-names.json'
+
+const all = json
+
+const random = num => {
+  let names
+  if (num) {
+    names = []
+    for (let i = 0; i < num; i++) {
+      names.push(unique(all)())
+    }
+  } else {
+    names = unique(all)()
+  }
+  return names
+}
+
+const names = {
+  all,
+  random,
+}
+
+export default names
---
+++ src/index.test.js
@@ -0,0 +1,18 @@
+import names from './index.js'
+
+describe('star-wars-names', () => {
+  it('can get all star wars names', () => {
+    const all = names.all;
+    expect(all.length).toEqual(93)
+    expect(all.includes('Yoda')).toBeTruthy()
+  })
+  it('can get a random star wars name', () => {
+    const name = names.random()
+    expect(names.all.includes(name)).toBeTruthy()
+  })
+  it('can get a random collection of star wars names', () => {
+    const collection = names.random(5)
+    expect(collection.length).toEqual(5)
+  })
+})
+
